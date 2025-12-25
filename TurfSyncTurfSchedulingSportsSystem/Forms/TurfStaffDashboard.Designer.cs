2bc98559f441026494077b35fb80aac08952fa48
Fairuz Faiyaz <fairuzfaiyaz2002@gmail.com>
2025-12-26 02:55:44 +0600

    turf staff dashboard

---
+++ TurfSyncTurfSchedulingSportsSystem/Forms/TurfStaffDashboard.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace TurfSyncTurfSchedulingSportsSystem.Forms
+{
+    partial class TurfStaffDashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button7 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.signUp = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button7
+            // 
+            this.button7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button7.BackColor = System.Drawing.Color.SeaGreen;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.Color.Lime;
+            //this.button7.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button7.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.button7.Location = new System.Drawing.Point(1040, 183);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(215, 56);
+            this.button7.TabIndex = 24;
+            this.button7.Text = "Start & End Match Timer";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button5.BackColor = System.Drawing.Color.SeaGreen;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Lime;
+            //this.button5.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button5.Location = new System.Drawing.Point(712, 326);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(215, 56);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Ground Preparation Status";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.BackColor = System.Drawing.Color.SeaGreen;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Lime;
+            //this.button4.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.button4.Location = new System.Drawing.Point(712, 183);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(215, 56);
+            this.button4.TabIndex = 22;
+            this.button4.Text = " Customer Arrival Check";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.Lime;
+            //this.button2.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button2.Location = new System.Drawing.Point(1040, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(215, 56);
+            this.button2.TabIndex = 21;
+            this.button2.Text = " Equipment Handling";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button6.BackColor = System.Drawing.Color.SeaGreen;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Lime;
+            //this.button6.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button6.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button6.Location = new System.Drawing.Point(1040, 502);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(215, 56);
+            this.button6.TabIndex = 20;
+            this.button6.Text = "Report Field Problems";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.BackColor = System.Drawing.Color.SeaGreen;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.Lime;
+            //this.button3.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.Location = new System.Drawing.Point(712, 502);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(215, 56);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "Clean-Up Confirmation";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // signUp
+            // 
+            this.signUp.AutoSize = true;
+            this.signUp.BackColor = System.Drawing.Color.Transparent;
+            this.signUp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.signUp.Font = new System.Drawing.Font("Century Gothic", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.signUp.ForeColor = System.Drawing.Color.Lime;
+            //this.signUp.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources.bcd34658d2db4b6b6d2bc08dc4d822ac;
+            this.signUp.Location = new System.Drawing.Point(835, 80);
+            this.signUp.Name = "signUp";
+            this.signUp.Size = new System.Drawing.Size(314, 35);
+            this.signUp.TabIndex = 14;
+            this.signUp.Text = "Turf Staff Management";
+            this.signUp.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            //this.button1.BackgroundImage = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources.c9d6300f17eccfd00b95baf9fc5a4a45;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            //this.button1.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            //this.pictureBox1.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources._2c02cdf777e6c239ae53c5e49b8b9f86;
+            this.pictureBox1.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1313, 771);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            //this.pictureBox3.Image = global::TurfSyncTurfSchedulingSportsSystem.Properties.Resources.bcd34658d2db4b6b6d2bc08dc4d822ac;
+            this.pictureBox3.Location = new System.Drawing.Point(1, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(686, 771);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 25;
+            this.pictureBox3.TabStop = false;
+            // 
+            // TrufStaffDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1314, 772);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.signUp);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "TrufStaffDashboard";
+            this.Text = "TrufStuffDashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label signUp;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
