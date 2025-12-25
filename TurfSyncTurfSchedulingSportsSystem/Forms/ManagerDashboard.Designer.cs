78e91bfe2dd2a5111f8c830164e77efecf03fae8
junajemi <junaidajemi@gmail.com>
2025-12-26 00:33:05 +0600

    add panel and label

--- TurfSyncTurfSchedulingSportsSystem/Forms/ManagerDashboard.Designer.cs
+++ TurfSyncTurfSchedulingSportsSystem/Forms/ManagerDashboard.Designer.cs
@@ -28,12 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            this.labeltitle = new System.Windows.Forms.Label();
+            this.panelHeader.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.BackColor = System.Drawing.Color.DarkGreen;
+            this.panelHeader.Controls.Add(this.labeltitle);
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(800, 60);
+            this.panelHeader.TabIndex = 0;
+            // 
+            // labeltitle
+            // 
+            this.labeltitle.AutoSize = true;
+            this.labeltitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labeltitle.ForeColor = System.Drawing.Color.White;
+            this.labeltitle.Location = new System.Drawing.Point(187, 9);
+            this.labeltitle.Name = "labeltitle";
+            this.labeltitle.Size = new System.Drawing.Size(398, 135);
+            this.labeltitle.TabIndex = 0;
+            this.labeltitle.Text = "Turf Manager Dashboard\r\n\r\n\r\n";
+            // 
+            // ManagerDashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelHeader);
+            this.Name = "ManagerDashboard";
             this.Text = "ManagerDashboard";
+            this.panelHeader.ResumeLayout(false);
+            this.panelHeader.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelHeader;
+        private System.Windows.Forms.Label labeltitle;
     }
 }
