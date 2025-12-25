513eb6ffa1bc47a9320f1a044d4326fdb3c7528b
Fairuz Faiyaz <fairuzfaiyaz2002@gmail.com>
2025-12-25 23:28:52 +0600

    added button

--- TurfSyncTurfSchedulingSportsSystem/Forms/PlayerDashboard.Designer.cs
+++ TurfSyncTurfSchedulingSportsSystem/Forms/PlayerDashboard.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TurfSyncTurfSchedulingSportsSystem.Forms
 {
-    partial class UserDashboard
+    partial class PlayerDashboard
     {
         /// <summary>
         /// Required designer variable.
@@ -29,37 +29,36 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(656, 191);
+            this.button1.Location = new System.Drawing.Point(699, 170);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // pictureBox1
+            // button2
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(340, 139);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(330, 264);
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.button2.Location = new System.Drawing.Point(747, 243);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
-            // UserDashboard
+            // PlayerDashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.pictureBox1);
+            this.ClientSize = new System.Drawing.Size(1346, 655);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Name = "UserDashboard";
-            this.Text = "UserDashboard";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.Name = "PlayerDashboard";
+            this.Text = "PlayerDashboard";
             this.ResumeLayout(false);
 
         }
@@ -67,6 +66,6 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
