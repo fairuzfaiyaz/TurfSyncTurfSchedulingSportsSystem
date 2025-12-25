d79a32b619adb25f79a1a7a16b60f2e75fcfcfb9
junajemi <junaidajemi@gmail.com>
2025-12-26 00:36:23 +0600

    main login with manger

--- TurfSyncTurfSchedulingSportsSystem/Program.cs
+++ TurfSyncTurfSchedulingSportsSystem/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TurfSyncTurfSchedulingSportsSystem.Forms;
 
 namespace TurfSyncTurfSchedulingSportsSystem
 {
@@ -18,6 +19,8 @@ namespace TurfSyncTurfSchedulingSportsSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginPage());
+            Application.Run(new ManagerDashboard());
+
         }
     }
 }
